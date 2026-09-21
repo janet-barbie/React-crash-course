ab2fd0f3d374fde06bcd4f8551ac6fd67e5a5bed
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-21 06:43:05 +0200

    click events

--- react-app/src/Button.jsx
+++ react-app/src/Button.jsx
@@ -1,17 +1,24 @@
 
-function Button(){
-    const styles ={
+// function Button(){
+//     const styles ={
         
-            backgroundColor: "hsl(200,100%,50%)",
-            color: "white",
-            padding: "10px 20px",
-            borderRadius: "5px",
-            border: "none",
-            cursor: "pointer"
+//             backgroundColor: "hsl(200,100%,50%)",
+//             color: "white",
+//             padding: "10px 20px",
+//             borderRadius: "5px",
+//             border: "none",
+//             cursor: "pointer"
     
-    }
+//     }
 
-    return<button style={styles}>click me</button>
+//     return<button style={styles}>click me</button>
   
+// }
+// export default Button
+
+function Button(){
+    let count = 0
+    const handleClick = (e) => e.target.textContent = "OUCH!"
+    return(<button onDoubleClick={(e)=> handleClick(e)}>click me</button>)
 }
 export default Button
