028c5a393ac7e3be2b0386d12b6dd4c53d176b14
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-22 21:34:49 +0200

    color picker exercise

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -9,12 +9,13 @@ import List from './List.jsx'
 import ProfilePicture from './ProfilePicture.jsx'
 import MyComponent from './MyComponent.jsx'
 import Counter from './Counter.jsx'
+import ColorPicker from './ColorPicker.jsx'
 function App() {
  
 return(
  <>
  {/* <Counter/> */}
-<MyComponent/>
+<ColorPicker/>
  </>
   
 )
