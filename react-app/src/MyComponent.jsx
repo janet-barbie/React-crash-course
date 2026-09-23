476617d199d6dd7badb4cb4331e3cd4507eec5b3
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-23 07:02:35 +0200

    how to update the state of an object using react

--- react-app/src/MyComponent.jsx
+++ react-app/src/MyComponent.jsx
@@ -1,53 +1,29 @@
-import React , {useState} from "react"
+import React, {useState} from 'react';
+
 function MyComponent(){
-    const [name,setName] = useState("Guest")
-    const [quantity,setQuantity] = useState(1)
-    const [comment,setComment] = useState("")
-    const [payment,setPayment] = useState("")
-    const [shipping,setShipping] = useState("Delivery")
-
-    function handleNameChange(event){
-        setName(event.target.value)
-    }
-    function handleQuantityChange(event){
-        setQuantity(event.target.value);
-    }
-    function handleCommentChange(event){
-        setComment(event.target.value)
-    } 
-    function handlePaymentChange(event){
-        setPayment(event.target.value)
+    const [car,setCar] = useState({year:2024,
+                                   make:"Ford",
+                                   model:"Mustang"})
+    function handleYearChange(event){
+        setCar(c => ({...c,year: event.target.value}))
 
     }
-    function handleShippingChange(event){
-        setShipping(event.target.value)
-    }
+    function handleMakeChange(event){
+        setCar(c => ({...c,make:event.target.value}))
+
+    }   
+    function handleModelChange(event){
+        setCar(c => ({...c,model:event.target.value}))
+
+    }                           
     return(
         <div>
-            <input value={name} onChange={handleNameChange} />
-            <p>Name: {name}</p>
-            <input value={quantity} onChange={handleQuantityChange} type="number"/>
-            <p>Quantity: {quantity}</p>
-            <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery instructions"/>
-            <p>Comment: {comment}</p>
-            <select value={payment} onChange={handlePaymentChange}>
-                <option value="">Select an option</option>
-                <option value="visa">Visa</option>
-                <option value="Mastercard">Mastercard</option>
-                <option value="Giftcard">Giftcard</option>
-            </select>
-            <p>Payment: {payment}</p>
-            <label>
-                <input type = "radio" value="pick up" checked={shipping === "pick up"} onChange={handleShippingChange}/>
-                pick up
-            </label><br/>
-            <label>
-                <input type="radio" value ="delivery" checked={shipping === "delivery"} onChange={handleShippingChange}/>
-                delivery
-            </label>
-            <p>Shipping: {shipping}</p>
+            <p>Your favorite car is: {car.year}  {car.make}  {car.model}</p>
+            <input type="number" value={car.year} onChange={handleYearChange}/> <br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
         </div>
-    )
+    )                               
+  
 }
-
-export default MyComponent;
+export default MyComponent
