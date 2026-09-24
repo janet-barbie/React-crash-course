1d8fbdb7fb5d0f70e8dac07d2b03b0283213c4ba
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-24 19:33:26 +0200

    useEffect hook part 2

--- react-app/src/MyComponent.jsx
+++ react-app/src/MyComponent.jsx
@@ -1,28 +1,33 @@
 import React,{useState,useEffect} from "react";
 
 function MyComponent(){
-    const [count,setCount] = useState(0);
-    const [color,setColor] = useState("green")
-    useEffect(()=>{
-        document.title = `Count: ${count} ${color}`
-    },[count,color])
+const [width ,setWidth] = useState(window.innerWidth);
+const [height,setHeight] = useState(window.innerHeight);
+useEffect(()=>{
+    window.addEventListener("resize",handleResize);
+    console.log("EVENT LISTENER ADDED")
+    return() =>{
+        window.removeEventListener("resize",handleResize);
+        console.log("Event Listener Removed")
+    }
+},[]);
+useEffect(()=>{
+    document.title = `Size: ${width} x ${height}`;
 
-    function addCount(){
-        setCount(c => c + 1);
-    }
-    function subtractCount(){
-        setCount( c => c - 1);
-    }
-    function changeColor(){
-        setColor(c => c === "green" ? "red":"green")
-    }
-    return(<>
-    <p style={{color:color}}>Count: {count}</p>
-    <button onClick={addCount}>Add</button>
-    <button onClick={subtractCount}>Subtract</button><br/>
-    <button onClick={changeColor}>Change Color</button>
-     
-    </>)
+},[width,height])
+
+function handleResize(){
+    setWidth(window.innerWidth)
+    setHeight(window.innerHeight)
+}
+    return(
+    <>
+    <p>Window width: {width}px</p>
+    <p>window Height: {height}px</p>
+ 
+    </>
+    )
+
 
 }
 export default MyComponent;
