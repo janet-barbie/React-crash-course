cbadd52cd6482e1e406e20d30b6a8ecb20c5563a
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-25 14:50:36 +0200

    how to use useRef

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
  
 return(
  <>
-<ComponentA/>
+<MyComponent/>
  </>
   
 )
--- react-app/src/MyComponent.jsx
+++ react-app/src/MyComponent.jsx
@@ -1,33 +1,54 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from 'react'
 
 function MyComponent(){
-const [width ,setWidth] = useState(window.innerWidth);
-const [height,setHeight] = useState(window.innerHeight);
-useEffect(()=>{
-    window.addEventListener("resize",handleResize);
-    console.log("EVENT LISTENER ADDED")
-    return() =>{
-        window.removeEventListener("resize",handleResize);
-        console.log("Event Listener Removed")
+   // let [number,setNumber] = useState(0)
+   const inputRef1 = useRef(null)
+   const inputRef2 = useRef(null)
+   const inputRef3 = useRef(null)
+  
+
+    useEffect(()=>{
+        console.log("COMPONENT RENDERED");
+
+    })
+    function handleClick1(){
+         inputRef1.current.focus();
+         inputRef1.current.style.backgroundColor = "yellow"
+         inputRef2.current.style.backgroundColor = "";
+         inputRef3.current.style.backgroundColor = ""
     }
-},[]);
-useEffect(()=>{
-    document.title = `Size: ${width} x ${height}`;
 
-},[width,height])
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef3.current.style.backgroundColor = ""
+   }
 
-function handleResize(){
-    setWidth(window.innerWidth)
-    setHeight(window.innerHeight)
+   function handleClick3(){
+    inputRef3.current.focus();
+    inputRef1.current.style.backgroundColor = ""
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "yellow"
 }
     return(
-    <>
-    <p>Window width: {width}px</p>
-    <p>window Height: {height}px</p>
- 
-    </>
-    )
+        <div>
+             <button onClick = {handleClick1}>
+            click me!
+        </button>
+        <input ref={inputRef1} />
 
+        <button onClick = {handleClick2}>
+            click me!
+        </button>
+        <input ref={inputRef2} />
 
-}
+        <button onClick = {handleClick3}>
+            click me!
+        </button>
+        <input ref={inputRef3} />
+        </div>
+       
+
+)}
 export default MyComponent;
