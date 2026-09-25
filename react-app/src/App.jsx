ea6c4be540ab0a7c2c976cf02cbf79dc8073889a
Janet Barbie <uwimanajanet563@gmail.com>
2026-09-25 16:44:23 +0200

    stopwatch app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,23 +1,9 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
-import Card from './Card.jsx'
-import Button from './Button.jsx'
-import Student from './Student.jsx'
-import UserGreeting from './UserGreeting.jsx'
-import List from './List.jsx'
-import ProfilePicture from './ProfilePicture.jsx'
-import MyComponent from './MyComponent.jsx'
-import Counter from './Counter.jsx'
-import ColorPicker from './ColorPicker.jsx'
-import TodoList from './TodoList.jsx'
-import DigitalClock from './DigitalClock.jsx'
-import ComponentA from './ComponentA.jsx'
+import Stopwatch from './Stopwatch.jsx'
 function App() {
  
 return(
  <>
-<MyComponent/>
+<Stopwatch/>
  </>
   
 )
